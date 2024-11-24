715c079c128c9a5deddb69e3ccc01a19e0bf95c3
20123933@tafe.wa.edu.au <20123933@tafe.wa.edu.au>
2024-11-24 14:18:55 +0800

    redo

--- src/car_park.py
+++ src/car_park.py
@@ -2,14 +2,5 @@
 
 
 class CarPark:
-    def __init__(self, location='Unknown', capacity, plates=None, sensors=None, displays=None):
-        self.location = location
-        self.capacity = capacity
-        self.sensors = sensors or []  # uses the first value if not None, otherwise uses the second value
-        self.plates = plates or []
-        self.displays = displays or []
-
-    def __str__(self):
-        carpark_info = f"Car Park at {location}, with {capacity}"
-        return carpark_info
+    pass
 
--- src/sensor.py
+++ src/sensor.py
@@ -2,19 +2,5 @@
 
 
 class Sensor:
-    def __init__(self, id, is_active=False, car_park="Unknown"):
-        self.id = id
-        self.is_active = is_active
-        self.car_park = car_park
-
-    def __str__(self):
-        sensor_info = f"Sensor of {id} at {car_park} with activity status of {is_active}"
-        return sensor_info
-
-
-class EntrySensor(Sensor):
     pass
 
-
-class ExitSensor(Sensor):
-    pass
