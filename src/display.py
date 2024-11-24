33ee2a90649a672615a191a0ada688699b39dcec
20123933@tafe.wa.edu.au <20123933@tafe.wa.edu.au>
2024-11-24 14:14:14 +0800

    add methods for display and sensor classes

--- src/display.py
+++ src/display.py
@@ -2,5 +2,12 @@
 
 
 class Display:
-    pass  # this just means nothing... it actually does nothing
-    # do this for ALL the other classes!
+    def __init__(self, id, message="", is_on=False, car_park="Unknown"):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        display_info = f"{id}: Welcome to the {car_park}"
+        return display_info
