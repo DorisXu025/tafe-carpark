d1d92b8abbcf124b0f9046e530f209cbe556f84b
20123933@tafe.wa.edu.au <20123933@tafe.wa.edu.au>
2024-11-24 14:21:04 +0800

    add method for display and sensor classes

--- src/display.py
+++ src/display.py
@@ -2,4 +2,12 @@
 
 
 class Display:
-    pass
+    def __init__(self, id, message="", is_on=False, car_park="Unknown"):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        display_info = f"{id}: Welcome to the {car_park}"
+        return display_info
