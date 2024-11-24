103fc1c565364fcd1b488ce3cc3da7db45744ce3
20123933@tafe.wa.edu.au <20123933@tafe.wa.edu.au>
2024-11-24 14:00:21 +0800

    add __init__ method for carpark

--- src/car_park.py
+++ src/car_park.py
@@ -2,5 +2,14 @@
 
 
 class CarPark:
-    pass # this just means nothing... it actually does nothing
-# do this for ALL the other classes!
+    def __init__(self, location='Unknown', capacity, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.sensors = sensors or []  # uses the first value if not None, otherwise uses the second value
+        self.plates = plates or []
+        self.displays = displays or []
+
+    def __str__(self):
+        carpark_info = f"Car Park at {location}, with {capacity}"
+        return carpark_info
+
